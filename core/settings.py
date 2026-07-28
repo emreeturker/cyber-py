4396dfc632fe6a460dee66f304af299644547812
emreeturker <emreturker.contact@gmail.com>
2026-07-28 10:30:36 +0300

    Move DEBUG and ALLOWED_HOSTS to environment variables

--- core/settings.py
+++ core/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.100']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
 
 # Application definition
